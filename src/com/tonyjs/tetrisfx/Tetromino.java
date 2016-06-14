2e3c8ac4453d96d1459b7df59f925166030abc5b
Tony Saavedra <ajsaavedrax@gmail.com>
2016-06-14 00:21:15 -0700

    Refactored tetromino definition to include rotated blocks

--- src/com/tonyjs/tetrisfx/Tetromino.java
+++ src/com/tonyjs/tetrisfx/Tetromino.java
@@ -9,51 +9,180 @@ import java.util.ArrayList;
  */
 public class Tetromino {
     private static ArrayList<TetrominoDefinition> pieceSet;
+    private static TetrominoDefinition I;
+    private static TetrominoDefinition rotatedI;
+    private static TetrominoDefinition J;
+    private static TetrominoDefinition rotatedJ;
+    private static TetrominoDefinition rotatedJ2;
+    private static TetrominoDefinition rotatedJ3;
+    private static TetrominoDefinition L;
+    private static TetrominoDefinition rotatedL;
+    private static TetrominoDefinition rotatedL2;
+    private static TetrominoDefinition rotatedL3;
+    private static TetrominoDefinition O;
+    private static TetrominoDefinition S;
+    private static TetrominoDefinition rotatedS;
+    private static TetrominoDefinition T;
+    private static TetrominoDefinition rotatedT;
+    private static TetrominoDefinition rotatedT2;
+    private static TetrominoDefinition rotatedT3;
+    private static TetrominoDefinition Z;
+    private static TetrominoDefinition rotatedZ;
 
-    private static final TetrominoDefinition I = new TetrominoDefinition(new int[][]{
-            {0, 0, 0, 0},
-            {1, 1, 1, 1}
-    }, Color.CYAN);
+    public void setIBlocks() {
+        I = new TetrominoDefinition(new int[][]{
+                {0, 0, 0, 0},
+                {1, 1, 1, 1}
+        }, Color.CYAN);
 
-    private static final TetrominoDefinition J = new TetrominoDefinition(new int[][]{
-            {1, 0, 0},
-            {1, 1, 1}
-    }, Color.BLUE);
+        rotatedI = new TetrominoDefinition(new int[][]{
+                {1, 0},
+                {1, 0},
+                {1, 0},
+                {1, 0}
+        }, Color.CYAN);
+    }
 
-    private static final TetrominoDefinition L = new TetrominoDefinition(new int[][]{
-            {0, 0, 1},
-            {1, 1, 1}
-    }, Color.rgb(255, 94, 0));
+    public void setJBlocks() {
+        J = new TetrominoDefinition(new int[][]{
+                {1, 0, 0},
+                {1, 1, 1}
+        }, Color.BLUE);
 
-    private static final TetrominoDefinition O = new TetrominoDefinition(new int[][]{
-            {1, 1},
-            {1, 1}
-    }, Color.YELLOW);
+        rotatedJ = new TetrominoDefinition(new int[][]{
+                {1, 1},
+                {1, 0},
+                {1, 0}
+        }, Color.BLUE);
 
-    private static final TetrominoDefinition S = new TetrominoDefinition(new int[][]{
-            {0, 1, 1},
-            {1, 1, 0}
-    }, Color.GREEN);
+        rotatedJ2 = new TetrominoDefinition(new int[][]{
+                {1, 1, 1},
+                {0, 0, 1}
+        }, Color.BLUE);
 
-    private static final TetrominoDefinition T = new TetrominoDefinition(new int[][]{
-            {0, 1, 0},
-            {1, 1, 1}
-    }, Color.PURPLE);
+        rotatedJ3 = new TetrominoDefinition(new int[][]{
+                {0, 1},
+                {0, 1},
+                {1, 1}
+        }, Color.BLUE);
+    }
 
-    private static final TetrominoDefinition Z = new TetrominoDefinition(new int[][]{
-            {1, 1, 0},
-            {0, 1, 1}
-    }, Color.RED);
+    public void setLBlocks() {
+        L = new TetrominoDefinition(new int[][]{
+                {0, 0, 1},
+                {1, 1, 1}
+        }, Color.rgb(255, 94, 0));
+
+        rotatedL = new TetrominoDefinition(new int[][]{
+                {1, 0},
+                {1, 0},
+                {1, 1}
+        }, Color.rgb(255, 94, 0));
+
+        rotatedL2 = new TetrominoDefinition(new int[][]{
+                {1, 1, 1},
+                {1, 0, 0}
+        }, Color.rgb(255, 94, 0));
+
+        rotatedL3 = new TetrominoDefinition(new int[][]{
+                {1, 1},
+                {0, 1},
+                {0, 1}
+        }, Color.rgb(255, 94, 0));
+    }
+
+    public void setOBlock() {
+        O = new TetrominoDefinition(new int[][]{
+                {1, 1},
+                {1, 1}
+        }, Color.YELLOW);
+    }
+
+    public void setSBlocks() {
+        S = new TetrominoDefinition(new int[][]{
+                {0, 1, 1},
+                {1, 1, 0}
+        }, Color.GREEN);
+
+        rotatedS = new TetrominoDefinition(new int[][]{
+                {1, 0},
+                {1, 1},
+                {0, 1}
+        }, Color.GREEN);
+    }
+
+    public void setTBlocks() {
+        T = new TetrominoDefinition(new int[][]{
+                {0, 1, 0},
+                {1, 1, 1}
+        }, Color.PURPLE);
+
+        rotatedT = new TetrominoDefinition(new int[][]{
+                {1, 0},
+                {1, 1},
+                {1, 0}
+        }, Color.PURPLE);
+
+        rotatedT2 = new TetrominoDefinition(new int[][]{
+                {1, 1, 1},
+                {0, 1, 0}
+        }, Color.PURPLE);
+
+        rotatedT3 = new TetrominoDefinition(new int[][]{
+                {0, 1},
+                {1, 1},
+                {0, 1}
+        }, Color.PURPLE);
+    }
+
+    public void setZBlocks() {
+        Z = new TetrominoDefinition(new int[][]{
+                {1, 1, 0},
+                {0, 1, 1}
+        }, Color.RED);
+
+        rotatedZ = new TetrominoDefinition(new int[][]{
+                {0, 1},
+                {1, 1},
+                {1, 0}
+        }, Color.RED);
+    }
 
     public Tetromino() {
+        setIBlocks();
+        setJBlocks();
+        setLBlocks();
+        setOBlock();
+        setSBlocks();
+        setTBlocks();
+        setZBlocks();
+
         pieceSet = new ArrayList<>();
         pieceSet.add(I);
+        pieceSet.get(0).setNext(rotatedI);
+
         pieceSet.add(J);
+        pieceSet.get(1).setNext(rotatedJ);
+        pieceSet.get(1).setNext(rotatedJ2);
+        pieceSet.get(1).setNext(rotatedJ3);
+
         pieceSet.add(L);
+        pieceSet.get(2).setNext(rotatedL);
+        pieceSet.get(2).setNext(rotatedL2);
+        pieceSet.get(2).setNext(rotatedL3);
+
         pieceSet.add(O);
+
         pieceSet.add(S);
+        pieceSet.get(4).setNext(rotatedS);
+
         pieceSet.add(T);
+        pieceSet.get(5).setNext(rotatedT);
+        pieceSet.get(5).setNext(rotatedT2);
+        pieceSet.get(5).setNext(rotatedT3);
+
         pieceSet.add(Z);
+        pieceSet.get(6).setNext(rotatedZ);
     }
 
     public TetrominoDefinition copy(TetrominoDefinition definition) {
@@ -67,10 +196,17 @@ public class Tetromino {
     public static class TetrominoDefinition {
         private final Color color;
         private final int[][] matrix;
+        private ArrayList<TetrominoDefinition> next;
+        private int currentIndex = 0;
 
         private TetrominoDefinition(int[][] matrix, Color color) {
             this.matrix = matrix;
             this.color = color;
+            this.next = new ArrayList<TetrominoDefinition>();
+        }
+
+        public void setNext(TetrominoDefinition nextItem) {
+            this.next.add(nextItem);
         }
 
         public int[][] getMatrix() {
@@ -79,6 +215,17 @@ public class Tetromino {
 
         public Color getColor() {
             return color;
+        }
+
+        public int[][] rotate() {
+            if (currentIndex != next.size()) {
+                int[][] toReturn = next.get(currentIndex).matrix;
+                currentIndex++;
+                return toReturn;
+            } else {
+                currentIndex = 0;
+                return matrix;
+            }
         }
     }
 }
