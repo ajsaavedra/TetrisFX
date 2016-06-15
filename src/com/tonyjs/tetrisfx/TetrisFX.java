d21d5f6d3935b1fc9abc7b92db1e9e8196467b47
Tony Saavedra <ajsaavedrax@gmail.com>
2016-06-15 15:18:40 -0700

    Rows can be deleted and shifted

--- src/com/tonyjs/tetrisfx/TetrisFX.java
+++ src/com/tonyjs/tetrisfx/TetrisFX.java
@@ -1,38 +1,44 @@
 package com.tonyjs.tetrisfx;
 
-import javafx.animation.AnimationTimer;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.Effect;
 import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
  * Created by tonysaavedra on 6/12/16.
  */
 public class TetrisFX extends Application {
-    public static final int TILE_SIZE = 40;
+    public static final int TILE_SIZE = 32;
     public static final int WIDTH = 10;
-    public static final int HEIGHT = 20;
+    public static final int HEIGHT = 24;
     public static boolean GAME_OVER = false;
     public static boolean ROTATING = false;
     public static boolean DROPPING = false;
     public static boolean SHIFTING = false;
-    private double time;
+    private double time, shiftTime;
+    private Timeline timeline;
 
     private AnimationTimer timer;
 
     private int tetrominoY = 0;
     private int tetrominoX = 0;
+    private int shiftDifference = 0;
 
     public Tetromino originalSet;
     public ArrayList<Tetromino.TetrominoDefinition> tetrominoSet;
@@ -45,7 +51,7 @@ public class TetrisFX extends Application {
 
     private Group tileGroup = new Group();
 
-    Lighting lighting = new Lighting(new Light.Distant(225, 55, Color.WHITE));
+    private Lighting lighting = new Lighting(new Light.Distant(225, 55, Color.WHITE));
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -75,7 +81,6 @@ public class TetrisFX extends Application {
                 moveRight();
             } else if (e.getCode() == KeyCode.DOWN) {
                 dropBrick();
-//                render();
             }
 
             ROTATING = false;
@@ -151,9 +156,9 @@ public class TetrisFX extends Application {
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                time += 0.030;
+                time += 0.015;
                 if (time >= 0.5) {
-                    if (!ROTATING && !SHIFTING || !DROPPING) {
+                    if (!ROTATING && !SHIFTING && !DROPPING) {
                         moveDown();
                         render();
                     }
@@ -174,6 +179,7 @@ public class TetrisFX extends Application {
         } else if (tetrominoY >= (HEIGHT - selectedMatrix.length) || !moveIsLegal) {
             timer.stop();
             DROPPING = false;
+            checkRows();
             respawn();
         } else {
             for (int i = 0; i < selectedMatrix.length ; i++) {
@@ -198,7 +204,6 @@ public class TetrisFX extends Application {
                     board[tetrominoY + i][tetrominoX + j].setAvailable(false);
                 }
             }
-//            oldTetrominoY += i;
         }
     }
 
@@ -227,6 +232,66 @@ public class TetrisFX extends Application {
         time = 0;
         timer = null;
         spawnTetrominos();
+    }
+
+    private void checkRows() {
+        boolean fullRow = true;
+        ParallelTransition deleteRowTransition = new ParallelTransition();
+
+        for (int i = tetrominoY + selectedMatrix.length-1; i >= tetrominoY; i--) {
+            fullRow = true;
+            for (int j = 0; j < WIDTH; j++) {
+                if (board[i][j].isAvailable()) {
+                    fullRow = false;
+                }
+            }
+            if (fullRow) {
+                shiftDifference++;
+                deleteRowTransition.getChildren().add(deleteRow(i, shiftDifference));
+            }
+        }
+
+        if (deleteRowTransition.getChildren().size() > 0) {
+            deleteRowTransition.play();
+            shiftDifference = 0;
+        }
+    }
+
+    private Animation deleteRow(int rowIndex, int shiftDifferenceX) {
+        ParallelTransition parallelTransition = new ParallelTransition();
+        for (int i = rowIndex; i >= rowIndex; i--) {
+            for (int j = 0; j < WIDTH; j++) {
+                if (i > 0) {
+                    if (i == rowIndex && board[i][j] != null) {
+                        final int colIndex = j;
+                        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.2), board[i][j]);
+                        fadeTransition.setToValue(0);
+                        fadeTransition.setCycleCount(2);
+                        fadeTransition.setAutoReverse(true);
+                        fadeTransition.setOnFinished(e -> {
+                            board[rowIndex][colIndex].setFill(Color.BLACK);
+                            board[rowIndex][colIndex].setEffect(null);
+                            board[rowIndex][colIndex].setAvailable(true);
+                            parallelTransition.getChildren().clear();
+                                for (int k = rowIndex-1; k > tetrominoY + selectedMatrix.length - 1; k--) {
+                                    if (!board[k][colIndex].isAvailable()) {
+                                        Paint color = board[k][colIndex].getFill();
+                                        board[k][colIndex].setFill(Color.BLACK);
+                                        board[k][colIndex].setEffect(null);
+                                        board[k][colIndex].setAvailable(true);
+                                        board[k + shiftDifferenceX][colIndex].setFill(color);
+                                        board[k + shiftDifferenceX][colIndex].setEffect(lighting);
+                                        board[k + shiftDifferenceX][colIndex].setAvailable(false);
+                                    }
+                                }
+                        });
+                        parallelTransition.getChildren().add(fadeTransition);
+                    }
+
+                }
+            }
+        }
+        return parallelTransition;
     }
 
     private Parent getContent() {
